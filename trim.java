7fc420c1436f31c3501d056e703916626ffff4d9
vishalfromvirali <vishalfromvirali@gmail.com>
2024-11-07 01:04:51 +0530

    Add files via upload

---
+++ trim.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class trim{
+    public static void main(String[]args){
+        String str1 = new  String();
+        str1 =str1+"            hello vishal cheif";
+        
+        String str2 = new String("vishal");
+        String result1 = str1.trim();
+
+        System.out.println(result1);
+
+        
+}
+}
